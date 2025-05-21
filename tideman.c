4bf8258493b9c6e347034534b7c409b7c4e74d71
DedPip <125252491+DedPip@users.noreply.github.com>
2025-05-21 07:34:01 +0330

    Add add_pairs to identify winning candidate pairs
    This function compares each pair of candidates based on the number of
    voters who prefer one over the other using the preferences[][] matrix.
    For each pair where one candidate is preferred over the other, a new
    pair is added to the pairs[] array with the appropriate winner and
    loser. The total number of pairs is tracked with pair_count.

--- tideman.c
+++ tideman.c
@@ -104,6 +104,10 @@ int main(int argc, string argv[])
     // }
 
     add_pairs();
+    // for (int i = 0; i < pair_count; i++)
+    // {
+    //     printf("Winner: %d\nLoser: %d\n", pairs[i].winner, pairs[i].loser);
+    // }
     sort_pairs();
     lock_pairs();
     print_winner();
@@ -139,8 +143,29 @@ void record_preferences(int ranks[])
 
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
-{
-    // TODO
+{  
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = i + 1; j < candidate_count; j++)
+        {
+            if (preferences[i][j] > preferences[j][i])
+            {
+                pairs[pair_count].winner = i;
+                pairs[pair_count].loser = j;
+                pair_count++;
+            }
+            else if (preferences[i][j] < preferences[j][i])
+            {
+                pairs[pair_count].winner = j;
+                pairs[pair_count].loser = i;
+                pair_count++;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
     return;
 }
 
